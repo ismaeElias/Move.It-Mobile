de22d11bbb1319277f21204c38b0ac57c58cb02f
IsmaeElias <ismael.eliass22@gmail.com>
2021-03-03 23:35:58 -0300

    Criado ExperienceBar

--- src/View/Home/index.js
+++ src/View/Home/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import {View, Text,StyleSheet} from 'react-native';
+import ExperienceBar from '../../components/ExperienceBar';
 function Home(){
 
   return(
     <View style={styles.container}>
-      <Text>Home</Text>
+      <ExperienceBar/>
     </View>
   );
 }
---
+++ src/components/ExperienceBar/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+function ExperienceBar() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.colorText}>0 exp</Text>
+        <View style={styles.experienceBar}>
+          <View style={{width: '50%', backgroundColor: 'green', height: 10}}></View>
+          <Text style={[styles.experience,{transform : [{ translateX: -20 }], left: '50%' }] }>50 exp</Text>
+        </View>
+      <Text style={styles.colorText}>600 exp</Text>
+    </View>
+  );
+}
+
+export default ExperienceBar;
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingLeft: 5,
+    paddingRight: 5,
+  },
+  experienceBar: {
+    backgroundColor: '#DCDDE0',
+    height: 10,
+    width: '70%',
+    position : 'relative'
+  },
+  colorText : {
+    color: '#666666'
+  },
+  experience : {
+    position : 'absolute',
+    top : 10,
+  }
+});
